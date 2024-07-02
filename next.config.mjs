6265f6e8c382e512352ef6b84642d4b3220fda08
nahuel <nahuelmellusso@gmail.com>
2024-07-02 11:00:18 -0300

    initial commit

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+import createNextIntPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default withNextIntl(nextConfig);
